96d46bdb9167a0e1e479b30bce65fdf5817dd4c2
AlbertDan <80307640+AlbertDan@users.noreply.github.com>
2021-05-26 21:43:48 +0300

    Update Proiect_Sincretic_SDA_Dan_Albert_Mircea.c

--- Proiect_Sincretic_SDA_Dan_Albert_Mircea.c
+++ Proiect_Sincretic_SDA_Dan_Albert_Mircea.c
@@ -7,74 +7,83 @@ int main()
     int M,N,i,j,*x,Q=0;
     do
     {
-    printf("M=");
-    scanf("%d",&M);
-    printf("N=");
-    scanf("%d",&N);
+        printf("M=");
+        scanf("%d",&M);
+        printf("N=");
+        scanf("%d",&N);
 
-    x=(int *)malloc(sizeof(int)*N*M);//alocare spatiu matrice
+        x=(int *)malloc(sizeof(int)*N*M);//alocare spatiu matrice
 
-    for(i=0; i < M*N; i++)//generare matrice
+        for(i=0; i < M*N; i++)//generare matrice
             x[i] = rand();
 
-    for(i=0; i < M*N; i++)//afisare matrice
+        for(i=0; i < M*N; i++)//afisare matrice
             printf("%d ",x[i]);
         printf("\n");
-    for(i=0; i < M; i++)
-        for(j=0; j < N; j++)//suma diag principala
-        {
-            if(i==j)
-            Q+=x[i*N+j];
-        }
+        for(i=0; i < M; i++)
+            for(j=0; j < N; j++)//suma diag principala
+            {
+                if(i==j)
+                    Q+=x[i*N+j];
+            }
 
-printf("\n"); printf("Suma Diag P=%d",Q);Q=0;
-
-    for(i=0; i < M; i++)
-        for(j=0; j < N; j++)//suma diag secundara
-        {
-            if(i+j==M-1)
-            Q+=x[i*N+j];
-        }
-
- printf("\n"); printf("Suma Diag S=%d",Q);
-Q=0;
-  for(i=0; i < M; i++)
-        for(j=0; j < N; j++)//Max sub diag principala
-        {
-            if(i>j && Q < x[i*N+j])
-            Q=x[i*N+j];
-        }
-        printf("\n"); printf("Max Sub Diag P=%d",Q);
-        Q=NULL;
-  for(i=0; i < M; i++)
-        for(j=0; j < N; j++)//Min sub diag principala
-        {
-            if(Q==NULL)
-                Q=x[i*N+j];
-            if(i>j && Q > x[i*N+j])
-            Q=x[i*N+j];
-        }
-        printf("\n"); printf("Min Sub Diag P=%d",Q);
+        printf("\n");
+        printf("Suma Diag P=%d",Q);
         Q=0;
-  for(i=0; i < M; i++)
-        for(j=0; j < N; j++)//Max sub diag secundara
-        {
-            if(i+j>M-1 && Q < x[i*N+j])
-            Q=x[i*N+j];
-        }
-        printf("\n"); printf("Max Sub Diag S=%d",Q);
+
+        for(i=0; i < M; i++)
+            for(j=0; j < N; j++)//suma diag secundara
+            {
+                if(i+j==M-1)
+                    Q+=x[i*N+j];
+            }
+
+        printf("\n");
+        printf("Suma Diag S=%d",Q);
+        Q=0;
+        for(i=0; i < M; i++)
+            for(j=0; j < N; j++)//Max sub diag principala
+            {
+                if(i>j && Q < x[i*N+j])
+                    Q=x[i*N+j];
+            }
+        printf("\n");
+        printf("Max Sub Diag P=%d",Q);
         Q=NULL;
-  for(i=0; i < M; i++)
-        for(j=0; j < N; j++)//Min sub diag secundara
-        {
-            if(Q==NULL)
-                Q=x[i*N+j];
-            if(i+j>M-1 && Q > x[i*N+j])
-            Q=x[i*N+j];
-        }
-        printf("\n"); printf("Min Sub Diag S=%d",Q);
- free(x);
- printf("\nGenerare matrice noua? 1-Da 0-Nu\n");scanf("%d",&Q);
-    }while(Q!=0);
+        for(i=0; i < M; i++)
+            for(j=0; j < N; j++)//Min sub diag principala
+            {
+                if(Q==NULL)
+                    Q=x[i*N+j];
+                if(i>j && Q > x[i*N+j])
+                    Q=x[i*N+j];
+            }
+        printf("\n");
+        printf("Min Sub Diag P=%d",Q);
+        Q=0;
+        for(i=0; i < M; i++)
+            for(j=0; j < N; j++)//Max sub diag secundara
+            {
+                if(i+j>M-1 && Q < x[i*N+j])
+                    Q=x[i*N+j];
+            }
+        printf("\n");
+        printf("Max Sub Diag S=%d",Q);
+        Q=NULL;
+        for(i=0; i < M; i++)
+            for(j=0; j < N; j++)//Min sub diag secundara
+            {
+                if(Q==NULL)
+                    Q=x[i*N+j];
+                if(i+j>M-1 && Q > x[i*N+j])
+                    Q=x[i*N+j];
+            }
+        printf("\n");
+        printf("Min Sub Diag S=%d",Q);
+        free(x);
+        printf("\nGenerare matrice noua? 1-Da 0-Nu\n");
+        scanf("%d",&Q);
+    }
+    while(Q!=0);
     return 0;
 }
